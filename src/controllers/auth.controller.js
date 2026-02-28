9b3cc31d2a7b86f7ba7b45a3bcebfe73f57c3f8f
Mayur Babbe <babbemayur45@gmail.com>
2026-02-28 17:25:59 +0530

    fixed cookies issue

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -127,12 +127,12 @@ async function userLoginController(req, res) {
         const token = generateToken(user._id);
 
         // 5️⃣ Secure cookie
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 3 * 24 * 60 * 60 * 1000
-        });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true, 
+                sameSite: 'none', 
+                maxAge: 3 * 24 * 60 * 60 * 1000 // 3 days
+            });
 
         // 6️⃣ Response
         return res.status(200).json({
